51a64819946b0430eec81e0dc894e982a82bc49d
Gusar82 <rusgusa82@gmail.com>
2022-04-12 01:27:20 +0500

    task No.2 completed

--- cook.py
+++ cook.py
@@ -9,7 +9,7 @@ def dish(file):
     for ingredient in range(count_ingredient):
         ingredient_name, quantity, measure = file.readline().strip().split(" | ")
         ingredients.append({'ingredient_name': ingredient_name,
-                            'quantity': quantity,
+                            'quantity': int(quantity),
                             'measure': measure})
     return name_dish, ingredients
 
@@ -25,6 +25,33 @@ def file_to_dict(file_path: str, cook_dict: dict):
                 return
 
 
+def get_shop_list_by_dishes(dishes: list, person_count: int) -> dict:
+    shop_list = dict()
+    for food in dishes:
+        for ingredient in cook_book[food]:
+            ingredient_name = ingredient['ingredient_name']
+            if ingredient_name not in shop_list:
+                shop_list[ingredient_name] = {'measure': ingredient['measure'],
+                                              'quantity': ingredient['quantity']*person_count}
+            else:
+                shop_list[ingredient_name]['quantity'] += ingredient['quantity']*person_count
+    return shop_list
+
+
+def print_cook_book(book: dict):
+    for key, value in book.items():
+        print(f'{key} :')
+        for i in value:
+            print(i)
+        print("---------")
+
+
+def print_shop_list(shop_list: dict):
+    for key, value in shop_list.items():
+        print(f'{key} : {value}')
+    print('--------------')
+
+
 base_path = os.getcwd()
 file_dir = 'books'
 file_name = "recipes.txt"
@@ -36,7 +63,8 @@ file_to_dict(file_path_all, cook_book)
 
 print(file_path_all)
 
-for key, value in cook_book.items():
-    print(f"{key} : ")
-    for ingredient in value:
-        print(ingredient)
+print_cook_book(cook_book)
+
+print_shop_list(get_shop_list_by_dishes(["Запеченный картофель", "Омлет"], 2))
+print_shop_list(get_shop_list_by_dishes(['Омлет', 'Омлет'], 1))
+print_shop_list(get_shop_list_by_dishes(['Омлет'], 2))
