178c2d4b1aad9c4f7feae2abd659c6940f7d57d7
Gusar82 <rusgusa82@gmail.com>
2022-04-12 00:27:21 +0500

    task No.1 completed

--- cook.py
+++ cook.py
@@ -1,15 +1,28 @@
 import os
 
 
-def dish(name: str, count_ingredient: int, ingredients: list) -> dict:
-    """Создает словарь с одним ключом - name,
-     и значением - список словарей в количестве count_ingredient"""
-    pass
+def dish(file):
+    """ключ - name_dish и списком ингредиентов - ingredients"""
+    name_dish = file.readline().strip()
+    count_ingredient = int(file.readline().strip())
+    ingredients = list()
+    for ingredient in range(count_ingredient):
+        ingredient_name, quantity, measure = file.readline().strip().split(" | ")
+        ingredients.append({'ingredient_name': ingredient_name,
+                            'quantity': quantity,
+                            'measure': measure})
+    return name_dish, ingredients
 
 
 def file_to_dict(file_path: str, cook_dict: dict):
     """добавляет в словарь из файла"""
-    pass
+    with open(file_path, 'r', encoding='utf-8') as file:
+        while True:
+            key, list_ingredients = dish(file)
+            cook_dict[key] = list_ingredients
+            empty_line = file.readline()
+            if not empty_line:
+                return
 
 
 base_path = os.getcwd()
@@ -22,4 +35,8 @@ cook_book = dict()
 file_to_dict(file_path_all, cook_book)
 
 print(file_path_all)
-print(cook_book)
+
+for key, value in cook_book.items():
+    print(f"{key} : ")
+    for ingredient in value:
+        print(ingredient)
